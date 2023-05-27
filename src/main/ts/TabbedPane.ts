8d8a00512f006576ae3f1ed9dacc97f6bf423e4f
Yassuo Toda <yassuo.toda@receita.fazenda.gov.br>
2023-05-27 19:01:40 -0300

    1.108

--- src/main/ts/TabbedPane.ts
+++ src/main/ts/TabbedPane.ts
@@ -190,8 +190,10 @@ class CardContainer {
 			const cardComponent: HTMLElement = cardContainer.children[i] as HTMLElement;
 			if (i === selectedIndex) {
 				cardComponent.style.visibility = "";
+				/*
 				cardComponent.tabIndex = -1;
 				cardComponent.focus();
+				*/
 			} else {
 				cardComponent.style.visibility = "hidden";
 				cardComponent.removeAttribute("tabindex");
